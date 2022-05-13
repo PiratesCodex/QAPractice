bcf0ddefd5aee085b863d2bb15d85ccee83186a9
PiratesCodex <sagarpatil4323@gmail.com>
2022-05-13 11:48:34 +0530

    add excel data driven

--- TestNGDemo/src/testNGpractice/AnnotationsDemo.java
+++ TestNGDemo/src/testNGpractice/AnnotationsDemo.java
@@ -3,6 +3,7 @@ package testNGpractice;
 import static org.testng.Assert.assertEquals;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -23,22 +24,30 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import excelUtility.ExcelReader;
+
 public class AnnotationsDemo {
 
 	public WebDriver driver;
+	ExcelReader er;
 	public String url = "https://www.lambdatest.com/";
 
 	@BeforeSuite
-	public void setUp() {
+	public void setUp() throws IOException {
 		System.setProperty("webdriver.chrome.driver",
-				"D:\\ZsnrJavaWorkspace\\TestNGDemo\\BrowserDriver\\chromedriver.exe");
+				"C:\\Users\\SP67139\\git\\QAPractice\\TestNGDemo\\BrowserDriver\\chromedriver.exe");
 		driver = new ChromeDriver();
+		String path = "C:\\Users\\SP67139\\git\\QAPractice\\TestNGDemo\\DataSource\\TestNGData.xlsx";
+		er = new ExcelReader(path);
 		Reporter.log("The Browser is Open now");
 	}
 
 	@BeforeTest
 	public void profileSetup() {
 		driver.manage().window().maximize();
+		
+		//implicit wait 
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		System.out.println("The profile setup process is completed");
 
 	}
@@ -51,39 +60,56 @@ public class AnnotationsDemo {
 
 	@Test(priority = 3)
 	public void checkLogin() throws InterruptedException {
+		int totalrow = er.getRowCount(0);
+		for (int i = 0; i < totalrow; i++) {
 		driver.get("https://accounts.lambdatest.com/login");
 		Thread.sleep(2500);
-		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("User61@gmail.com");
+		String mail = er.getData(0, i, 1);
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(mail);
 		Reporter.log("The Username data \"User61@gmail.com\" is Entered");
 		Thread.sleep(2500);
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("TestUser123");
+		String pass = er.getData(0, i, 2);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(pass);
 		Reporter.log("The Password data \"TestUser123\" is Entered");
 		Thread.sleep(2500);
 		driver.findElement(By.id("login-button")).click();
 		Thread.sleep(2500);
 		System.out.println("The login process on lamdatest is completed");
+		}
 	}
 
 	@Test(priority = 1, description = "this test validates the sign-up test")
 	public void signUp() throws InterruptedException {
+		int totalrow = er.getRowCount(0);
+		for (int i = 0; i < totalrow; i++) {
+			System.out.println("***********************This " + i +"th index user infromation***************************\n");
+			
 		WebElement link = driver.findElement(By.xpath("//*[@id=\"header\"]/nav/div/div/div[2]/div/div/div[2]/a[2]"));
 		link.click();
 //		Thread.sleep(2500);
 //		WebElement organization = driver.findElement(By.xpath("//input[@name='organization_name']"));
 //		organization.sendKeys("LambdaTest");
 		Thread.sleep(2500);
-		WebElement firstName = driver.findElement(By.xpath("//input[@name='name']"));
-		firstName.sendKeys("Test123");
+		
+		String uname = er.getData(0, i, 0);
+		driver.findElement(By.xpath("//input[@name='name']")).sendKeys(uname);
+		System.out.println("user name is = " + uname);
 		Thread.sleep(2500);
-		WebElement email = driver.findElement(By.xpath("//input[@name='email']"));
-		email.sendKeys("User61@gmail.com");
+		
+		String mail = er.getData(0, i, 1);
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(mail);
+		System.out.println("user mail id is = " + mail);
 		Thread.sleep(2500);
-		WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
-		password.sendKeys("TestUser123");
+		
+		String pass = er.getData(0, i, 2);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(pass);
+		System.out.println("user password is = " +pass);
 		Thread.sleep(2500);
+		
 		WebElement phoneNumber = driver.findElement(By.xpath("//input[@name='phone']"));
 		phoneNumber.sendKeys("9412267690");
 		Thread.sleep(2500);
+		
 		WebElement termsOfService = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/form/div[5]/label/samp"));
 		termsOfService.click();
 		Thread.sleep(2500);
@@ -91,6 +117,7 @@ public class AnnotationsDemo {
 		button.click();
 		Thread.sleep(2500);
 		System.out.println("The sign-up process is completed");
+		}
 
 	}
 	
@@ -105,6 +132,9 @@ public class AnnotationsDemo {
 	public void testCurrentUrl() throws InterruptedException {
 	
 		String currentUrl = driver.getCurrentUrl();
+		
+		//Assertion determines the state of the application 
+		
 		assertEquals(currentUrl, "https://accounts.lambdatest.com/email/verify",
 				"url did not matched");
 		System.out.println("The url validation test is completed");
